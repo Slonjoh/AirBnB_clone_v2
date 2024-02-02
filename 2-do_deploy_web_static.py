3f6788e3d241ee26384a63e5367502ec8a5cd9d6
Slonjoh <tobson11122@gmail.com>
2024-02-02 05:55:00 -0800

    changed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,51 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-import os.path
+"""Fab file to archive web_static content"""
+from os import remove
+from os.path import exists
 from fabric.api import *
-from fabric.operations import run, put, sudo
+from datetime import datetime
 
 
 env.hosts = ['54.237.87.200', '52.86.189.30']
-# env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to a web server.
-    """
-    if (os.path.isfile(archive_path) is False):
+    """Deploy function for archive to get deployed to servers"""
+    if not exists(archive_path):
         return False
-
-    try:
-        new_comp = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
-        run("sudo rm /tmp/{}".format(new_comp))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
-        return True
-    except Exception as e:
-        print(f"Error during deployment: {e}")
+    fileNameExt = archive_path.split('/')[-1]
+    fileName = fileNameExt.split(".")[0]
+    result = put(archive_path, '/tmp/{}'.format(fileNameExt))
+    if result.failed:
         return False
+    result = run("sudo rm -rf /data/web_static/releases/{}/".format(fileName))
+    if result.failed:
+        return False
+    result = run("sudo mkdir -p /data/web_static/releases/{}/".format(fileName))
+    if result.failed:
+        return False
+    result = run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+                 .format(fileNameExt, fileName))
+    if result.failed:
+        return False
+    result = run("sudo rm /tmp/{}".format(fileNameExt))
+    if result.failed:
+        return False
+    input = "sudo mv /data/web_static/releases/{}/web_static/*\
+    /data/web_static/releases/{}/".format(fileName, fileName)
+    result = run(input)
+    if result.failed:
+        return False
+    result = run("sudo rm -rf /data/web_static/releases/{}/web_static"
+                 .format(fileName))
+    if result.failed:
+        return False
+    result = run("sudo rm -rf /data/web_static/current")
+    if result.failed:
+        return False
+    result = run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                 .format(fileName))
+    if result.failed:
+        return False
+    print("New version deployed!")
+    return True
