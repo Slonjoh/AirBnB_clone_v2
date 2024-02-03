3d9d888218899d14e8b6d947b609e374deac3d59
Slonjoh <tobson11122@gmail.com>
2024-02-03 13:18:33 +0000

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash                                                                                                                                      
 # Setup a web servers for the deployment of web_static.                                                                                                  
-sudo apt update -y                                                                                                                                       
-sudo apt install -y nginx                                                                                                                                
-sudo mkdir -p /data/web_static/releases/test/                                                                                                            
-sudo mkdir -p /data/web_static/shared/                                                                                                                   
-sudo echo "<!DOCTYPE html>                                                                                                                               
-<html>                                                                                                                                                   
-  <head>                                                                                                                                                 
-  </head>                                                                                                                                                
-  <body>                                                                                                                                                 
-    <p>Nginx server test</p>                                                                                                                             
-  </body>                                                                                                                                                
-</html>" | tee /data/web_static/releases/test/index.html                                                                                                 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current                                                                                     
-sudo chown -R ubuntu:ubuntu /data                                                                                                                        
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default                                                                                                             
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
